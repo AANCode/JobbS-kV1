7157efe0dd016c58599391f9062db72cdaafa1df
AANCode <122744819+AANCode@users.noreply.github.com>
2026-08-19 14:11:40 +0200

    hjelpe metoder

--- src/main/java/org/example/JobListing.java
+++ src/main/java/org/example/JobListing.java
@@ -1,4 +1,50 @@
 package org.example;
 
+import java.util.List;
+
 public class JobListing {
+
+    private String title;
+    private String company;
+    private String location;
+    private List<String> technologies;
+    private String url;
+    private String status;
+
+    public JobListing(String title, String company, String location, List<String> technologies, String url, String status) {
+        this.title = title;
+        this.company = company;
+        this.location = location;
+        this.technologies = technologies;
+        this.url = url;
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public List<String> getTechnologies() {
+        return technologies;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return "JobListing{" +
+                "title='" + title + '\'' +
+                ", company='" + company + '\'' +
+                ", location='" + location + '\'' +
+                ", technologies=" + technologies +
+                ", url='" + url + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,45 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        List<JobListing> jobs = new ArrayList<>();
+
+        JobListing job1 = new JobListing("Nyutdannet Teknologi 2027", "Sopra Steria", "Oslo", List.of("Java", "Spring", "PostgreSQL"), "*Senere", "søkt");
+        JobListing job2 = new JobListing("Nyutdannet utvikler 2027", "Capgemini", "Oslo", List.of("Java", "Spring", "PostgreSQL"), "*Senere", "søkt");
+        JobListing job3 = new JobListing("Junior utvikler", "Skynet", "Oslo", List.of(".Net", "C#", "PostgreSQL"), "*Senere", "Ikke søkt");
+        JobListing job4 = new JobListing("Junior utvikler", "NRK", "Trondheim", List.of("C", "SDL2", "PostgreSQL"), "*Senere", "Ikke søkt");
+        JobListing job5 = new JobListing("Junior utvikler", "Arasaka", "Oslo", List.of("Java", "Spring", "PostgreSQL"), "*Senere", "Ikke søkt");
+        JobListing job6 = new JobListing("Junior utvikler", "Arasaka", "CA", List.of("Java", "Spring", "PostgreSQL"), "*Senere", "Ikke søkt");
+        JobListing job7 = new JobListing("Senior utvikler", "Kongsbergs gruppen", "Kongsberg", List.of("Java", "Spring", "PostgreSQL"), "*Senere", "Ikke søkt");
+
+        jobs.add(job1);
+        jobs.add(job2);
+        jobs.add(job3);
+        jobs.add(job4);
+        jobs.add(job5);
+        jobs.add(job6);
+        jobs.add(job7);
+
+        for (JobListing job : jobs) {
+            if (isJuniorGraduate(job) && hasJava(job) && isNotApplied(job)) {
+                System.out.println(job);
+            }
         }
+    }
+
+    public static boolean isJuniorGraduate(JobListing job) {
+        return job.getTitle().contains("Junior") || job.getTitle().contains("Nyutdannet");
+    }
+
+    public static boolean hasJava(JobListing job) {
+        return job.getTechnologies().contains("Java");
+    }
+
+    public static boolean isNotApplied(JobListing job) {
+        return job.getStatus().equals("Ikke søkt");
     }
 }
